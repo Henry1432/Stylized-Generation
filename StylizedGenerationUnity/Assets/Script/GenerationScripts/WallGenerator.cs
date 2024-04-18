2c8b9f4cbee93aeba612f8007e0f6a7339c80985
AshleyLivingstoneTAC <michael.mcdonald@mymail.champlain.edu>
2024-04-18 11:16:11 -0400

    wall generation start

---
+++ StylizedGenerationUnity/Assets/Script/GenerationScripts/WallGenerator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGenerator : MonoBehaviour
+{
+    public GameObject wall;
+    public SplineGenerator splineGen;
+    // Start is called before the first frame update
+    void Start()
+    {
+       splineGen = FindObjectOfType<SplineGenerator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // PROBLEM!! O(n^2)
+        foreach(Spline s in splineGen.splines)
+        {
+            foreach(Vector3 point in s.points)
+            {
+                Instantiate(wall, point, s.transform.rotation);
+            }
+        }
+    }
+}
