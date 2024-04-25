c947f27c2609624160335414a48d4f50630737f1
AshleyLivingstoneTAC <michael.mcdonald@mymail.champlain.edu>
2024-04-25 03:51:15 -0400

    tube wraps around spline

--- StylizedGenerationUnity/Assets/Script/GenerationScripts/MeshCopy.cs
+++ StylizedGenerationUnity/Assets/Script/GenerationScripts/MeshCopy.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 using static UnityEngine.Rendering.HableCurve;
+using System.Linq;
+using System.Drawing;
 
 public class MeshCopy : MonoBehaviour
 {
@@ -18,9 +20,10 @@ public class MeshCopy : MonoBehaviour
     public Material mat;
     public Transform first;
 
-    int speed = 10;
+    int speed = 1;
+    int frames = 0;
     public float radius;
-    const int SEGMENTS = 50;
+    const int SEGMENTS = 25;
     bool ran = false;
     // Start is called before the first frame update
     void Start()
@@ -41,7 +44,7 @@ public class MeshCopy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!ran && splineGen.splines.Count != 0)
+        if (!ran && splineGen.splines.Capacity != 0 && frames >= 2)
         {
             int counter = 0;
             GameObject obj = new GameObject("Mesh", typeof(MeshFilter), typeof(MeshRenderer));
@@ -54,7 +57,7 @@ public class MeshCopy : MonoBehaviour
                     if (counter == speed)
                     {
                         up = Quaternion.FromToRotation(s.direction[(s.points.IndexOf(point) - 1)], s.direction[s.points.IndexOf(point)]) * up;
-                        verts.AddRange(CreateVerts(point, s.direction[s.points.IndexOf(point)], up, 1f, SEGMENTS));
+                        verts.AddRange(CreateVerts(point, s.direction[s.points.IndexOf(point)], up, 0.5f, SEGMENTS));
                        
                         counter = 0;
                     }
@@ -73,7 +76,7 @@ public class MeshCopy : MonoBehaviour
             obj.GetComponent<MeshRenderer>().material = mat;
             ran = true;
         }
-
+        frames++; // giving a two frame buffer allows closed splines to close
     }
 
     List<Vector3> CreateVerts(Vector3 center, Vector3 dir, Vector3 up, float radius, int segments)
@@ -88,7 +91,7 @@ public class MeshCopy : MonoBehaviour
             tempUp = pt.normalized;
         }
 
-        verts.RemoveAt(0);
+       verts.RemoveAt(0);
        
         return verts;
     }
@@ -96,38 +99,55 @@ public class MeshCopy : MonoBehaviour
     List<int> CreateIndices(List<Vector3> v)
     {
         List<int> indices = new List<int>();
-        for(int i = 0; i < v.Count - SEGMENTS; i++)
+        for(int i = 0; i < v.Count - SEGMENTS; i++) // real for loop
+        //for(int i = 0; i < SEGMENTS * 10; i++) // debug for loop
         {
-            // if final leg of segment - wrap around
+            // if final point of segment - wrap around to first point
             if((i + 1) % SEGMENTS == 0)
             {
-                indices.Add(i); //final point current seg
-                indices.Add(i + SEGMENTS); // final point on next seg
-                indices.Add(i - SEGMENTS + 1);// first point current seg
+
+                indices.Add(i + SEGMENTS);     // current point next seg
+                indices.Add(i);                // current point current seg
+                indices.Add(i - SEGMENTS + 1); // first point current seg
+                //indices.Add(i + SEGMENTS + 1); // first point next seg
+                //indices.Add(i + SEGMENTS);     // current point next seg
+                //indices.Add(i - SEGMENTS + 1); // first point current seg
             }
             else
             {
-                indices.Add(i); // current point current seg
-                indices.Add(i + SEGMENTS); // current point next seg
-                indices.Add(i + 1); // next point current seg
+                indices.Add(i + SEGMENTS);      // current point next seg
+                indices.Add(i);                 // current point current seg
+                indices.Add(i + 1);             // next point current seg
+                //indices.Add(i + SEGMENTS + 1);  // next point next seg
+                //indices.Add(i + SEGMENTS);      // current point next seg
+                //indices.Add(i + 1);             // point after current seg
             }
         }
-        // for final segment, wrap around to first
-        for(int j = v.Count - SEGMENTS; j < SEGMENTS; j++)
+        // for final segment, wrap around to first segment
+        for (int j = v.Count - SEGMENTS; j < v.Count - 2; j++)
         {
+            // if final point of segment - wrap around to first point
             if ((j + 1) % SEGMENTS == 0)
             {
-                indices.Add(j); //final point final seg
-                indices.Add(0); // first point first seg
-                indices.Add(j - SEGMENTS + 1);//first point final seg
+                indices.Add(0);                // first point first seg
+                indices.Add(j);                // final point final seg
+                indices.Add(j - SEGMENTS + 1); // first point final seg
+                //indices.Add(1);                // next  point first seg
+                //indices.Add(0);                // first point first seg
+                //indices.Add(j - SEGMENTS + 1); // first point final seg
+
             }
             else
             {
-                indices.Add(j); //start point final seg
-                indices.Add(j % SEGMENTS); //next point first seg
-                indices.Add(j + 1); // next point final seg
+                indices.Add(j % SEGMENTS);     // current point first seg 
+                indices.Add(j);                // current point final seg   
+                indices.Add(j + 1);            // next point final seg
+                //indices.Add(j + 1 % SEGMENTS); // next point first seg
+                //indices.Add(j % SEGMENTS);     // current point first seg
+                //indices.Add(j + 1);            // next point final seg
             }
         }
+
         return indices;
     }
 }
