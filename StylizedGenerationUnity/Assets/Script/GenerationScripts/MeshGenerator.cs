8b0002a51f7d58a8f1f19e8a212bbbd464b7d743
AshleyLivingstoneTAC <michael.mcdonald@mymail.champlain.edu>
2024-04-30 13:58:16 -0400

    Vertex/Index

--- StylizedGenerationUnity/Assets/Script/GenerationScripts/MeshGenerator.cs
+++ StylizedGenerationUnity/Assets/Script/GenerationScripts/MeshGenerator.cs
@@ -1,44 +1,220 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using UnityEngine.Rendering;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+using static UnityEngine.Rendering.HableCurve;
+using System.Linq;
+using System.Drawing;
+using static UnityEngine.Rendering.DebugUI.Table;
+using Unity.VisualScripting;
+using UnityEngine.UIElements;
+using System.Reflection;
 
-public class MeshGenerator : MonoBehaviour
+public class MeshCopy : MonoBehaviour
 {
-    public Vector3 minBounds;
-    public Vector3 maxBounds;
-    public int numPoints;
-    public float interval;
-    public SplinePoint point;
-    public SplineGenerator gen;
-    public List<SplinePoint> points;
+    public SplineGenerator splineGen;
+    public List<Vector3> verts;
+    public List<Vector2> uvs;
+    public List<int> triangles;
 
-    [Range(1, 25)]
-    public int speed;
+    public Material mat;
+    public Transform first;
+
+    int speed = 1;
+    int frames = 0;
+    public float radius;
+    const int SEGMENTS = 100;
+    bool ran = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        gen = GetComponentInChildren<SplineGenerator>();
-        for (int i = 0; i < numPoints; i++)
-        {
-            Vector3 randPos = new Vector3(Random.Range(minBounds.x, maxBounds.x), Random.Range(minBounds.y, maxBounds.y), Random.Range(minBounds.z, maxBounds.z));
-            Vector3 randRot = new Vector3(Random.Range(0, 180), Random.Range(0, 180), Random.Range(0, 180));
-            Quaternion randQ = new Quaternion(randRot.x, randRot.y, randRot.z, 0);
-            Instantiate(point, randPos, randQ);
-            point.prePoint = point.transform.GetChild(0);
-            point.postPoint = point.transform.GetChild(1);
-            points.Add(point);
-        }
-        gen.points = points;
-        for (int i = 0; i < numPoints - 1; i++)
-        {
-            gen.CreateSpline(points[i], points[i + 1]);
-        }
-
+        splineGen = FindObjectOfType<SplineGenerator>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (splineGen == null)
+        {
+            splineGen = FindObjectOfType<SplineGenerator>();
+        }
+        if (!ran && splineGen.splines.Capacity != 0 && frames >= 2)
+        {
+            GenerateMesh();
+        }
+        frames++; // giving a two frame buffer allows closed splines to close
+
+        if (Input.GetKeyDown(KeyCode.R) && ran)
+        {
+            Rerun();
+        }
+    }
+
+    List<Vector3> CreateVerts(Vector3 center, Vector3 dir, Vector3 up, float radius)
+    {
+        List<Vector3> verts = new List<Vector3>();
+        Vector3 tempUp = up;
+        Vector3 f = dir.normalized;
+        Vector3 r = Vector3.Cross(f, up).normalized;
+        Vector3 u = Vector3.Cross(r, f).normalized;
+        for (int i = 0; i <= SEGMENTS; i++)
+        {
+            float theta = (2 * Mathf.PI / SEGMENTS) * i;
+            Vector3 pos = center + r * Mathf.Cos(theta) + u * Mathf.Sin(theta);
+            verts.Add(pos);
+        }
+        //Vector3 randNoise = tempUp * UnityEngine.Random.Range(-1, 1);
+        //// Vector3 r = , Vector3 u = , Vector3 f = dir;
+        //verts.Add(center + (radius * tempUp) + randNoise);
+        //float theta = (360 / SEGMENTS) * i;
+        //Vector3 pt = Quaternion.AngleAxis(theta, dir) * tempUp;
+        //tempUp = pt.normalized;
+        //Vector3 first = verts.First();
+        //verts.RemoveAt(0);
+        //verts.Add(first);
+
+        return verts;
+    }
+
+    List<int> CreateIndices(List<Vector3> v)
+    {
+        List<int> indices = new List<int>();
+        int points = 0;
+        foreach (Spline s in splineGen.splines)
+        {
+            foreach (Vector3 p in s.points)
+            {
+                points++;
+            }
+        }
+        int columns = SEGMENTS + 1;
+        int rings = v.Count / SEGMENTS;
+        Debug.Log(points); Debug.Log(rings);
+
+        for (int row = 1; row < SEGMENTS - 1; row++)
+        {
+            for (int col = 0; col < SEGMENTS; col++)
+            {
+                int start = row * columns + col;
+                indices.Add(start);
+
+                indices.Add(start + 1);
+
+                indices.Add(start + columns);
+            }
+        }
+        return indices;
+    }
+
+    void GenerateMesh()
+    {
+        splineGen = FindObjectOfType<SplineGenerator>();
+        Mesh mesh = new Mesh();
+        int counter = 0;
+        GameObject obj = new GameObject("Mesh", typeof(MeshFilter), typeof(MeshRenderer));
+        Vector3 up = first.up;
+
+        foreach (Spline s in splineGen.splines)
+        {
+            foreach (Vector3 point in s.points)
+            {
+                if (counter == speed)
+                {
+                    //up = Quaternion.FromToRotation(s.direction[s.points.IndexOf(point) - 1], s.direction[s.points.IndexOf(point)]) * up;
+                    up = Vector3.up;
+                    verts.AddRange(CreateVerts(point, s.direction[s.points.IndexOf(point)], up, 2f));
+
+                    counter = 0;
+                }
+                else
+                {
+                    counter++;
+                }
+
+            }
+        }
+        triangles.AddRange(CreateIndices(verts));
+        mesh.vertices = verts.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.triangles = triangles.ToArray();
+        obj.GetComponent<MeshFilter>().mesh = mesh;
+        obj.GetComponent<MeshRenderer>().material = mat;
+        ran = true;
+    }
+    public void Rerun()
+    {
+        Mesh obj = FindObjectOfType<Mesh>();
+        Destroy(obj);
+        verts.Clear();
+        uvs.Clear();
+        triangles.Clear();
+        GenerateMesh();
     }
 }
+//for (int i = 0; i < v.Count; i++) // real for loop
+//for(int i = 0; i < SEGMENTS * 5; i++) // debug for loop
+//    //if final point of segment - wrap around to first point
+//    if ((i + 1) % SEGMENTS == 0)
+//    {
+
+//        indices.Add(i + SEGMENTS);     // current point next seg
+//        indices.Add(i);                // current point current seg
+//        indices.Add(i - SEGMENTS + 1); // first point current seg
+//        if (quads)
+//        {
+//            indices.Add(i + SEGMENTS + 1); // first point next seg
+//            indices.Add(i + SEGMENTS);     // current point next seg
+//            indices.Add(i - SEGMENTS + 1); // first point current seg
+//        }
+
+//    }
+//    else
+//    {
+//        indices.Add(i + SEGMENTS);      // current point next seg
+//        indices.Add(i);                 // current point current seg
+//        indices.Add(i + 1);             // next point current seg
+//        if (quads)                               
+//        {
+//           indices.Add(i + SEGMENTS + 1);  // next point next seg
+//           indices.Add(i + SEGMENTS);      // current point next seg
+//           indices.Add(i + 1);             // point after current seg
+//        }
+
+
+//    }
+//}
+//// for final segment, wrap around to first segment
+////for (int j = v.Count - SEGMENTS; j < v.Count; j++)
+////{
+////    // if final point of segment - wrap around to first point
+////    if ((j + 1) % SEGMENTS == 0)
+////    {
+////        indices.Add(0);                // first point first seg
+////        indices.Add(j);                // final point final seg
+////        indices.Add(j - SEGMENTS + 1); // first point final seg
+////        if(quads)
+////        {
+////           indices.Add(1);                // next  point first seg
+////           indices.Add(0);                // first point first seg
+////           indices.Add(j - SEGMENTS + 1); // first point final seg
+////        }
+
+
+////    }
+////    else
+////    {
+////        indices.Add(j % SEGMENTS);     // current point first seg 
+////        indices.Add(j);                // current point final seg   
+////        indices.Add(j + 1);            // next point final seg
+////        if(quads)
+////        {
+////            indices.Add(j + 1 % SEGMENTS); // next point first seg
+////            indices.Add(j % SEGMENTS);     // current point first seg
+////            indices.Add(j + 1);            // next point final seg          
+////        }
+////    }
+////}
